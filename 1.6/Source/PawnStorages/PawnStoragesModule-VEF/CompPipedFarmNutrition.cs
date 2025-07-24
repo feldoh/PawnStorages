6f07c93e90d07d1001e9a3c177fb02426c8f6a46
Kieran David Evans <keyz182@gmail.com>
2025-07-24 22:42:40 +0100

    Reapply "Switch piped farms to CompResourceStorage rather than just CompResource."
    This reverts commit f96151dc63945bd07e702b0382dd72e6af11237f.

--- 1.6/Source/PawnStorages/PawnStoragesModule-VEF/CompPipedFarmNutrition.cs
+++ 1.6/Source/PawnStorages/PawnStoragesModule-VEF/CompPipedFarmNutrition.cs
@@ -1,55 +1,91 @@
-﻿using PipeSystem;
+﻿using System;
+using PipeSystem;
 using RimWorld;
 using UnityEngine;
 using Verse;
 
 namespace PawnStorages.VEF;
 
-public class CompPipedPawnStorageNutrition: CompPawnStorageNutrition
+public class CompPipedPawnStorageNutrition : CompPawnStorageNutrition
 {
-    public CompResource _compResource;
+    public CompResourceStorage _compResource;
     public bool _haveCheckedForComp = false;
-    public virtual CompResource compResource
-    {
-        get
-        {
-            if (!_haveCheckedForComp && _compResource == null && parent.HasComp<CompResource>())
-            {
-                _compResource = parent.GetComp<CompResource>();
-                // make sure we only check once.
-                _haveCheckedForComp = true;
-            }
-            return _compResource;
-        }
-    }
+    private Lazy<CompResourceStorage> _resourceStorage;
 
-    public override bool IsPiped => IsAttachedToNet(out PipeNet pipeNet, out CompResource resource);
-    public override float storedNutrition => IsAttachedToNet(out PipeNet pipeNet, out CompResource resource) ? pipeNet.Stored : base.storedNutrition;
-    public override float MaxNutrition => IsAttachedToNet(out PipeNet pipeNet, out CompResource resource) ? pipeNet.AvailableCapacity : base.MaxNutrition;
+    public virtual CompResourceStorage ResourceStorage => _resourceStorage.Value;
 
-    public bool IsAttachedToNet(out PipeNet pipeNet, out CompResource resource)
+    public PipeNet pipeNet => ResourceStorage?.PipeNet;
+
+    public override bool IsPiped => true;
+    public override float storedNutrition => pipeNet?.Stored ?? 0;
+    public override float MaxNutrition => pipeNet?.AvailableCapacity ?? 0;
+
+    public override void Initialize(CompProperties props)
     {
-        pipeNet = null;
-        resource = compResource;
-        if (resource is not { PipeNet: { } net }) return false;
-        pipeNet = net;
-        return pipeNet.connectors.Count > 1;
+        base.Initialize(props);
+        _resourceStorage = new Lazy<CompResourceStorage>(() => parent?.GetComp<CompResourceStorage>());
     }
 
     public override bool AbsorbToFeedIfNeeded(Need_Food foodNeeds, float desiredFeed, out float amountFed)
     {
-        if (!IsAttachedToNet(out PipeNet pipeNet, out CompResource resource)) return base.AbsorbToFeedIfNeeded(foodNeeds, desiredFeed, out amountFed);
-
         amountFed = Mathf.Min(pipeNet.Stored, desiredFeed);
         pipeNet.DrawAmongStorage(amountFed, pipeNet.storages);
         return true;
     }
 
-    public override bool TryAbsorbNutritionFromHopper(float nutrition)
+    public override bool TryAbsorbNutritionFromHopper(float requestedNutrition)
     {
-        if (!IsAttachedToNet(out PipeNet pipeNet, out CompResource resource)) return base.TryAbsorbNutritionFromHopper(nutrition);
+        if (!IsValidNutritionRequest(requestedNutrition))
+            return false;
 
-        return !(pipeNet.Stored < nutrition);
+        Thing feedSource = FindFeedInAnyHopper();
+        if (feedSource == null)
+            return false;
+
+        float remainingNutrition = ProcessFeedAbsorption(feedSource, requestedNutrition);
+        float absorbedNutrition = requestedNutrition - remainingNutrition;
+
+        if (absorbedNutrition <= 0)
+            return false;
+
+        pipeNet.DistributeAmongStorage(absorbedNutrition, out float _);
+        return true;
     }
 
+    private bool IsValidNutritionRequest(float nutrition)
+    {
+        return nutrition > 0 && HasEnoughFeedstockInHoppers();
+    }
+
+    private float ProcessFeedAbsorption(Thing feedSource, float nutritionToAbsorb)
+    {
+        float remainingNutrition = nutritionToAbsorb;
+        float nutritionPerUnit = feedSource.GetStatValue(StatDefOf.Nutrition);
+
+        while (remainingNutrition > 0)
+        {
+            int unitsToAbsorb = Mathf.Min(feedSource.stackCount, Mathf.FloorToInt(remainingNutrition / nutritionPerUnit));
+
+            if (unitsToAbsorb <= 0)
+                break;
+
+            float nutritionAbsorbed = unitsToAbsorb * nutritionPerUnit;
+            if (nutritionAbsorbed <= 0)
+                break;
+
+            remainingNutrition -= nutritionAbsorbed;
+
+            if (unitsToAbsorb > 1)
+            {
+                feedSource.SplitOff(unitsToAbsorb);
+            }
+            else
+            {
+                feedSource.DeSpawn();
+                break;
+            }
+        }
+
+        return remainingNutrition;
+    }
 }
